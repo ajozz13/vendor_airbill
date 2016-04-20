d5c0e2fbaa5611164e146a4b69912eef4bdd0ad4
aochoa <aochoa@ajozz-mob2>
2016-04-20 13:14:33 -0400

    add support for printing airwaybills.

--- scripts/vendor-billapp.js
+++ scripts/vendor-billapp.js
@@ -79,33 +79,39 @@ function showModalMessage( alert, main_title, msg,  text_mode ){ // text_mode=tr
 	}
 	$("#modalMsgLabel").html( main_title );
 
-     $("#modalMsgContent").empty();
-     if( text_mode ){
-     	$("#modalMsgContent").html( msg );
-     }else{
-          var msg_obj = toJsonObject( msg );
-          $("#modalMsgInnerLabel").html( msg_obj[ 'message' ] );
-          $("#modalMsgLabel").append( " ("+msg_obj[ 'code' ]+ ")" );
-          var devMsg = $("<p></p>").html( msg_obj['developerMessage'].replace( /\n/g, "<br />") );
-          $("#modalMsgContent").append( "<hr />", devMsg );
-     }
+	$("#modalMsgContent").empty();
+	if( text_mode ){
+		$("#modalMsgContent").html( msg );
+	}else{
+		var msg_obj = toJsonObject( msg );
+		$("#modalMsgInnerLabel").html( msg_obj[ 'message' ] );
+		$("#modalMsgLabel").append( " ("+msg_obj[ 'code' ]+ ")" );
+		var devMsg = $("<p></p>").html( msg_obj['developerMessage'].replace( /\n/g, "<br />") );
+		$("#modalMsgContent").append( "<hr />", devMsg );
+	}
 	$('#modalMsg').modal('show');
 }
 
 $(function (){
+	$("#PrintBill").click( function(){
+		printContents( "billdata" );
+	});
 	//POST form submit
 	var rtype = "json"; //text or json
 	$( "#fsubmit" ).click( function(){
 		//alert($( "input[name=contents]:checked" ).val());	
 		var btn = $(this);
 		if( $( "#bill_form" ).valid() ){
-			$.post( "https://api.pactrak.com/ibcairbill/production",  { 'billdata':buildJson() }, function( data ) {
+			var jsonInput = buildJson();
+			$.post( "https://api.pactrak.com/ibcairbill/production",  { 'billdata':jsonInput }, function( data ) {
 				rtype === "text" ? handleTextResponse( data, "test_response" ) :
-					alertJSON( JSON.stringify( data ), "test_response" );
+				console.log( data ); 
+				presentBill( toJsonObject( jsonInput ), data.trackingNumber );
+				$( '#modalPrint' ).modal('show');
 			},rtype )
 			.fail( function( data ) {
 				rtype === "text" ? handleTextResponse( data.responseText, "test_response" ) :
-                         showModalMessage(true, "Failure Message - Issue storing your information", data.responseText, false);
+			showModalMessage(true, "Failure Message - Issue storing your information", data.responseText, false);
 			});
 			return false;
 		}
@@ -127,6 +133,17 @@ $(function (){
 		val_a = Globalize.parseFloat( Globalize.format( val_a, "c" ) );
 		return $("#contents").val() === 'APX' ? ( val_a >= 1 ) : ( val_a <= 1 );
 	}," -DOC 1 or 0, APX >= 1" );
+	$.validator.addMethod("dimension_match", function( value, element, param ) {
+		var flg = true;
+		var val_d = $("#dimensions").val();
+		console.log( val_d  );
+		if( val_d ){
+			var m = val_d.match( /(\d+\s?x\s?\d+\s?x\s?\d+)/i );
+			console.log( m );
+			return m;
+		}
+		return flg;
+	}," L x  W x H" );
 	$('#bill_form').validate({
 		rules: {  
 		account: { required: true, maxlength: 4 }, shipper_name: { required: true, maxlength: 35 }, shipper_phone:{ required: true, rangelength: [10, 25] },
@@ -138,7 +155,8 @@ $(function (){
 		consignee_country:{required: true, maxlength: 2 }, consignee_company_name:{ required: false, maxlength: 35 }, 
 		consignee_state:{ required: false, rangelength: [2, 20] },consignee_zip:{ required: false, maxlength: 20 },
 		description:{ required: true, maxlength: 20 }, weight:{ required: true, number: true }, pieces: {required: true, number: true},
-		value:{ required: function(e) { return $("#contents").val() === 'APX'; }, number: true, document_value: true }, insurance: { required: true, max: 2500, number: true }
+		value:{ required: function(e) { return $("#contents").val() === 'APX'; }, number: true, document_value: true }, insurance: { required: true, max: 2500, number: true },
+		dimensions: { required: false, dimension_match: true }
 		}, messages: { account:" -4 digits" , shipper_state: " -2-20 Max.", shipper_zip: " -2-20 Max."}, 
 		errorElement: "span",
 		errorClass: 'help-block',
@@ -151,7 +169,79 @@ $(function (){
 
 });
 
+function presentBill( entry, trackNumber ){
+	$("#shipmentInfo").empty();
+	var url = $( "#trak_image" ).attr( "location" )+trackNumber;
+	$( "#trak_image" ).attr( "src", url );
+	$( "#trk" ).text( trackNumber );
+	$( "#s1" ).text( joinMe( " - ", entry.shipper.account, entry.shipper.name, entry.shipper.company_name ) );
+	$( "#s2" ).text( entry.shipper.address );
+	$( "#s3" ).text( joinMe( " ", entry.shipper.city, entry.shipper.state, entry.shipper.zip ) );
+	$( "#s4" ).text( joinMe( " ", entry.shipper.country, entry.shipper.phone  ) );
+	$( "#c1" ).text( joinMe( " ",entry.consignee.name, entry.consignee.company_name ) );
+	$( "#c2" ).text( entry.consignee.address );
+	$( "#c3" ).text( joinMe( " ", entry.consignee.city, entry.consignee.state, entry.consignee.zip, entry.consignee.country  ) );
+	$( "#c4" ).text( entry.consignee.phone );
+	//$( "#c5" ).text(  );
+	var d = new Date();
+	createLabelElement( "Date: ", joinMe( " ",  d.toDateString(), d.getHours()+":"+d.getMinutes() ), '#shipmentInfo'  );
+	createLabelElement( "Value: ",  entry.shipment.value , '#shipmentInfo'  );
+	createLabelElement(  "Weight: ",  joinMe( " ", entry.shipment.weight, entry.shipment.weight_qualifier ) , '#shipmentInfo'   );
+	createLabelElement( "Type: ", entry.shipment.contents, '#shipmentInfo'  );
+	createLabelElement( "Pieces: ", entry.shipment.pieces , '#shipmentInfo'  );
+	createLabelElement( "Description: ", entry.shipment.description , '#shipmentInfo'  );
+	createLabelElement( "Packaging: ", entry.shipment.packaging, '#shipmentInfo'  );
+	createLabelElement( "Service: ", entry.shipment.service, '#shipmentInfo'  );
+	createLabelElement( "Payment: ", entry.shipment.bill_to, '#shipmentInfo'  );
+
+	if( entry.shipper.reference ){
+		createLabelElement( 'Reference:', entry.shipper.reference, '#shipmentInfo' );
+	}
+	if( entry.shipment.insurance_amount ){
+		createLabelElement( 'Insurance $', entry.shipment.insurance_amount, '#shipmentInfo' );
+	}
+	if( entry.shipment.platinum_service ){
+		createLabelElement( 'Platinum:', entry.shipment.platinum_service, '#shipmentInfo' );
+	}
+	if( entry.shipment.dimensions ){
+		createLabelElement( 'Dims:', joinMe( " ", entry.shipment.dimensions, entry.shipment.dimensions_qualifier ), '#shipmentInfo' );
+	}
+}
+function joinMe() { //joinMe( "-" a, b, c ) will yield  "a-b-c"
+	var result = '';
+	var sep = arguments[ 0 ];
+	if( arguments.length > 1 ){
+		for( i = 1; i < arguments.length; i++ ) {
+			if ( arguments[i] === undefined ){
+				result += " ";
+			}else{
+				result += arguments[i];
+			}
+			if( i !== arguments.length -1 ){
+				result += sep;
+			}
+		}
+	}
+	return result;
+} 
+function createLabelElement( label, value, target ){
+	var dv = $("<div></div>");
+	var txt1 = $("<label></label>").text( label );
+	var txt2 = $("<span></span>").text( value ); 
+	dv.append( txt1 ).append( txt2 );
+	$( target ).append( dv );     // Append new elements
+}
+
 //Utilities
 function toJsonObject( json_input ){
 	return JSON && JSON.parse( json_input ) || $.parseJSON( json_input );
+}
+function printContents(id){
+		var mywindow = window.open('', 'tracking div', 'height=400,width=600'); 
+		mywindow.document.write('<html><head><title>Web Airwaybill</title><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"><link rel="stylesheet" href="css/application.css" media="print" /><link rel="stylesheet" href="css/bootstrap-print.min.css" media="print"/></head><body onload="window.print()">');
+		mywindow.document.write($("#"+id).html());
+		mywindow.document.write('</body></html>');
+		mywindow.document.close(); // necessary for IE >= 10
+		mywindow.focus(); // necessary for IE >= 10
+		setTimeout(function(){mywindow.close();}, 1000); 
 }
